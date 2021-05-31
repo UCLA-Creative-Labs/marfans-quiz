b96fe2f36a25737ec1cc31ac3b8ab54492a77620
Arjun Raghavan <arjun.raghavan.2k@gmail.com>
2021-05-31 13:26:18 -0700

    Moved questions to separate file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,12 @@
 import React from 'react';
 import Carousel from '../components/Carousel';
 import Layout from '../components/Layout';
-import { PROJECT, Question } from '../utils';
-
-const questions: Question[] = [
-  {
-    question: 'How do we feel about Minecraft?',
-    answers: [
-      {
-        answer: 'I only know Minetest.',
-        projects: [PROJECT.LARK],
-      },
-      {
-        answer: 'I was homeschooled',
-        projects: [PROJECT.LARK],
-      },
-      {
-        answer: 'No thanks, I had friends in middle school.',
-        projects: [PROJECT.LARK],
-      },
-      {
-        answer: '🎵 dust of the redstone 🎵',
-        projects: [PROJECT.LARK],
-      },
-    ],
-  },
-];
+import { QUESTIONS } from '../utils/questions';
 
 export default function Home(): JSX.Element {
   return (
     <Layout>
-      <Carousel questions={questions}/>
+      <Carousel questions={QUESTIONS}/>
     </Layout>
   );
 }
---
+++ utils/questions.ts
@@ -0,0 +1,25 @@
+import { PROJECT, Question } from './';
+
+export const QUESTIONS: Question[] = [
+  {
+    question: 'How do we feel about Minecraft?',
+    answers: [
+      {
+        answer: 'I only know Minetest.',
+        projects: [PROJECT.LARK],
+      },
+      {
+        answer: 'I was homeschooled',
+        projects: [PROJECT.LARK],
+      },
+      {
+        answer: 'No thanks, I had friends in middle school.',
+        projects: [PROJECT.LARK],
+      },
+      {
+        answer: '🎵 dust of the redstone 🎵',
+        projects: [PROJECT.LARK],
+      },
+    ],
+  },
+];
