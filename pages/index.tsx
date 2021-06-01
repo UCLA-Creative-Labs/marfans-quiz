870faa78638d1a75902adf264c421a5a0c6d012d
Arjun Raghavan <arjun.raghavan.2k@gmail.com>
2021-05-31 17:48:56 -0700

    Persist shuffle order across refreshes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,26 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useState } from 'react';
 import Carousel from '../components/Carousel';
 import Layout from '../components/Layout';
-import { QUESTIONS } from '../utils';
+import { QUESTIONS, Question } from '../utils';
 import { shuffle } from '../utils/array';
 
 export default function Home(): JSX.Element {
-  const questions = useRef(QUESTIONS);
-  console.log(QUESTIONS);
+  const [questions, setQuestions] = useState<Question[]>([]);
 
-  return (
+  useEffect(() => {
+    const storage = window.sessionStorage;
+    const shuffled = storage.getItem('shuffled');
+
+    if (shuffled) setQuestions(JSON.parse(shuffled) as Question[]);
+    else {
+      const shuffledQs = shuffle(QUESTIONS);
+      storage.setItem('shuffled', JSON.stringify(shuffledQs));
+    }
+  }, []);
+
+  return questions.length > 0 ? (
     <Layout>
-      <Carousel questions={questions.current}/>
+      <Carousel questions={questions}/>
     </Layout>
-  );
+  ) : <></>; // TODO: Add loading indicator? Might not be necessary since it's so short.
 }
